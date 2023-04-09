07db39bf421fd18277e4452039f66dcaff322b16
ravi <samcollins9899@gmail.com>
2023-04-09 20:30:14 +0530

    added caching with react query

--- src/useBreedList.jsx
+++ src/useBreedList.jsx
@@ -1,48 +1,16 @@
-import {useState,useEffect} from 'react';
+import { useQueries, useQuery } from "@tanstack/react-query";
+import fetchBreedList from "./fetchBreedList";
 
-const localCache = {};
+
 
 
 
 export default function useBreedList(animal)
 {
-    const [breedList, setBreedList] = useState([]);
-
-    const [status, setStatus] = useState("unloaded");
+   const results = useQuery(["breeds", animal, fetchBreedList]);
 
 
-    useEffect( function getBreedList()
-    { 
-        if(!animal)
-        {
-            setBreedList([])
-        }else if(localCache[animal])
-        {
-            setBreedList(localCache[animal]);
-        }
-        else {
-            requestBreedList();
 
-        }     
-        
-        async function requestBreedList()
-        {
-            setBreedList([]);
-
-            setStatus("Loading");
-
-          const res = await fetch(`http://pets-v2.dev-apis.com/breeds?animal=${animal}`);
-
-          const json = await res.json();
-          localCache[animal]= json.breeds || {};
-          setBreedList(localCache[animal]);
-          setStatus("loaded");
-
-        }
-    } , [animal])
-    
-
-
-    return [breedList,status];
+    return [results ?.data?.breeds ?? [], results.status];
 }
 
